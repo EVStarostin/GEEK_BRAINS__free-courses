da0d2aa3a7e55226f6fe572b6e63cb614a344614
Evgenii Starostin <e.starostin89@gmail.com>
2018-06-01 20:28:06 +0300

    done!

--- task2.js
+++ task2.js
@@ -1,17 +1,17 @@
 /* Task 2 */
+/*
+    2. Количество дней до нового года
+    Рассчитать количество дней до нового года.
+    На вход поадется дата в формате d m y
+    1 <= d <= 31
+    m in [января февраля марта апреля мая июня июля августа сентября октября ноября декабря]
+    1 <= y <= 2999
 
+    Пример
+    > node task.js 4 июля 2018
+    > 180
+*/
 /* Put your code here */
-
-/* 2. Количество дней до нового года
-Рассчитать количество дней до нового года.
-На вход поадется дата в формате d m y
-1 <= d <= 31
-m in [января февраля марта апреля мая июня июля августа сентября октября ноября декабря]
-1 <= y <= 2999
-
-Пример
-> node task.js 4 июля 2018
-> 180 */
 
 var d = parseFloat(process.argv[2]),
     m = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'].indexOf(process.argv[3]),
@@ -21,6 +21,6 @@ var inputDate = new Date(y, m, d);
 var newYear = new Date(inputDate.getFullYear()+1, 0, 1);
 
 var daysUntilNewYear = (newYear - inputDate)/86400000 - 1;
-var result = String(daysUntilNewYear);
+var result = daysUntilNewYear.toString();
 
 process.stdout.write((result));
--- task3.js
+++ task3.js
@@ -1,14 +1,40 @@
 /* Task 3 */
+/*
+    3. Сложение временных промежутков
+    Сложить 2 временных промежутка и вывести общую длительность в человекочитаемом виде.
+    В выводе использовать часы, минуты и секунды (при необходимости).
+    На вход подается два числа t1 и t2, разделенные пробелом. 1 <= t1, t2 <= 100000
 
+    Пример
+    > node task.js 1 1
+    > 2 секунды
+    > node task.js 3600 3599
+    > 1 час 59 минут 59 секунд
+*/
 /* Put your code here */
 
-/* 3. Сложение временных промежутков
-Сложить 2 временных промежутка и вывести общую длительность в человекочитаемом виде.
-В выводе использовать часы, минуты и секунды (при необходимости).
-На вход подается два числа t1 и t2, разделенные пробелом. 1 <= t1, t2 <= 100000
+var t1 = parseFloat(process.argv[2]),
+    t2 = parseFloat(process.argv[3]);
 
-Пример
-> node task.js 1 1
-> 2 секунды
-> node task.js 3600 3599
-> 1 час 59 минут 59 секунд */
+var hoursLabels    = ['час', 'часа', 'часов'],
+    minutesLabels  = ['минута', 'минуты', 'минут'],
+    secondsLabels  = ['секунда', 'секунды', 'секунд'];
+
+function declOfNum(number, titles)  
+{  
+    var cases = [2, 0, 1, 1, 1, 2];  
+    if (number !== 0) {
+        return number + ' ' + titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
+    } else {
+        return '';
+    }
+}
+
+var t = t1 + t2;
+var h = parseInt(t / 3600);
+var m = parseInt((t - h*3600) / 60);
+var s = parseInt(t - h*3600 - m*60);
+
+var result = declOfNum(h, hoursLabels) + ' ' + declOfNum(m, minutesLabels) + ' ' + declOfNum(s, secondsLabels);
+
+process.stdout.write(result);
