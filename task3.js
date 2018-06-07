4cb87b65428c8e5076973b70cf3ba910cc98993e
Evgenii Starostin <e.starostin89@gmail.com>
2018-06-07 08:34:55 +0300

    выложил 3 задание

---
+++ task3.js
@@ -0,0 +1,34 @@
+var t1 = parseFloat(process.argv[2]),
+    t2 = parseFloat(process.argv[3]);
+
+var hoursLabels    = ['час', 'часа', 'часов'],
+    minutesLabels  = ['минута', 'минуты', 'минут'],
+    secondsLabels  = ['секунда', 'секунды', 'секунд'];
+
+function declOfNum(number, titles)  
+{  
+    var cases = [2, 0, 1, 1, 1, 2];  
+    return number + ' ' + titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
+}
+
+var t = t1 + t2;
+var h = Math.floor(t / 3600);
+var m = Math.floor((t - h*3600) / 60);
+var s = Math.floor(t - h*3600 - m*60);
+
+var result = '';
+if (h !== 0) {
+  result += declOfNum(h, hoursLabels);
+}
+if (m !== 0 && result !== '') {
+  result += ' ' + declOfNum(m, minutesLabels);
+} else if (m !== 0) {
+  result += declOfNum(m, minutesLabels);
+}
+if (s !== 0 && result !=='') {
+  result += ' ' + declOfNum(s, secondsLabels)
+} else if (s !== 0) {
+  result += declOfNum(s, secondsLabels)
+}
+
+process.stdout.write(result);
