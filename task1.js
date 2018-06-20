1643aa0f5fb4a93177ebd3bd98a7ab08d6392390
Evgenii Starostin <e.starostin89@gmail.com>
2018-06-20 22:28:42 +0300

    добавил описание задачи

--- task1.js
+++ task1.js
@@ -1,3 +1,15 @@
+/* Task 1 */
+/* 
+    Палиндром
+    Написать функцию, возвращающую YES, если входная строка является палиндромом и NO в противном случае.
+    На вход подается строка ASCII символов длины n. 1 <= n <= 10000
+
+    Пример
+    > node task.js 'Was it a car or a cat I saw?'
+    > YES
+*/
+/* Put your code here */
+
 var str = process.argv[2];
 var normalStr = str.replace(/\W|\_/g, "").toLowerCase();
 var reverseStr = normalStr.split("").reverse().join("");
