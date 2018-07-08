1844e3d91cd3b157a442f1c417ddce8856d6a166
Evgenii Starostin <e.starostin89@gmail.com>
2018-07-08 18:54:15 +0300

    -

--- js1/lesson 5/home work/main.js
+++ js1/lesson 5/home work/main.js
@@ -1,5 +1,7 @@
 function getIATA() {
-  var data = null;
+  info.innerHTML = `<pre>
+    Загрузка ...
+  <pre>`
 
   var xhr = new XMLHttpRequest();
 
@@ -15,11 +17,10 @@ function getIATA() {
 
   xhr.open("GET", "https://www.travelpayouts.com/widgets_suggest_params?q=%D0%98%D0%B7%20%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D1%8B%20%D0%B2%20%D0%9D%D0%B8%D1%86%D1%86%D1%83");
 
-  xhr.send(data);
+  xhr.send();
 }
 
 function getPrices(originIATA, destinationIATA, callback) {
-  var data = null;
 
   var xhr = new XMLHttpRequest();
 
@@ -31,17 +32,22 @@ function getPrices(originIATA, destinationIATA, callback) {
 
   xhr.open("GET", `http://min-prices.aviasales.ru/calendar_preload?origin=${originIATA}&destination=${destinationIATA}&depart_date=2018-09-01&one_way=true`);
 
-  xhr.send(data);
+  xhr.send();
 }
 
 function findTheCheapestTicket(tickets) {
   tickets = tickets.filter( (ticket) => ticket.depart_date.match(/(2018)-((09)|(10)|(11))-\d\d/g) );
   const theCheapestTicket = tickets.sort( (a, b) => a.value - b.value )[0];
   console.log(`
-    The cheapest ticket from Moscow to Nice:
+    The cheapest ticket from Moscow to Nice in autumn 2018:
     price: ${theCheapestTicket.value}
     depart_date: ${theCheapestTicket.depart_date}
   `);
+  info.innerHTML = `<pre>
+    The cheapest ticket from Moscow to Nice in autumn 2018:
+    price: ${theCheapestTicket.value}
+    depart date: ${theCheapestTicket.depart_date}
+  <pre>`
 }
 
 getIATA();
