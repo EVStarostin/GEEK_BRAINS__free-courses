522ba1f2b28c227e0805aff273e65efc07b1a7e5
Evgenii Starostin <e.starostin89@gmail.com>
2018-07-10 00:39:14 +0300

    -

--- js1/lesson 6/home work/index.js
+++ js1/lesson 6/home work/index.js
@@ -1,3 +1,12 @@
+/*
+  1. Игра в выживание птичек. Цель игры: выяснить, какая птичка съест больше всего своих сородичей.
+  Алгоритм решения:
+  1. Создайте класс Bird.
+  2. Добавьте птичке свойства name, points, wasEaten.
+  3. Создайте 10 птичек с именами Bird 1, Bird 2, ..... Bird 3.
+  4. Запустите цикл: до тех пор, пока не останется только одна живая птичка. Цикл должен выбирать случайным образом одну из живых птиц и скармливать ей любую другую (та, которая съедена, становится wasEaten=true, а та, которую покормили, — point++).
+*/
+
 class Bird {
 
   constructor(name) {
@@ -36,7 +45,7 @@ for (let i = 1; i <= 10; i++) {
 
 const birdsAlive = birds.slice();
 
-while ( birdsAlive.filter( bird => bird.wasEaten === false ).length > 1 ) {
+while ( birdsAlive.length > 1 ) {
   const rand = Math.floor(Math.random() * (birdsAlive.length - 0)) + 0;
   birdsAlive[rand].eat(birdsAlive);
 }
