c89f245f9ce73958b679b6bd7616e5fda0473a6b
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-13 12:23:37 +0300

    -

---
+++ js2/lesson 3/home work/index.js
@@ -0,0 +1,4 @@
+var quotesRegExp = /(\W')|('\W)/;
+var qoutesString = "Большой 'текст', где используются 'кавычки', но в слове aren’t 'кавычки' не 'кавычки'.";
+var phoneRegExp = /^\+7\(\d{3}\)\d{3}-\d{4}$/;
+var phoneString = "+7(927)099-6701";
