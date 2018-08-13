6e1158d306a8c1227daad23f872085d551f46b14
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-13 12:31:23 +0300

    -

--- js2/lesson 3/home work/index.js
+++ js2/lesson 3/home work/index.js
@@ -1,4 +1,8 @@
 var quotesRegExp = /(\W')|('\W)/;
 var qoutesString = "Большой 'текст', где используются 'кавычки', но в слове aren’t 'кавычки' не 'кавычки'.";
 var phoneRegExp = /^\+7\(\d{3}\)\d{3}-\d{4}$/;
-var phoneString = "+7(927)099-6701";
+var phone = "+7(927)099-6701";
+var emailRegExp = /^\w+(\.|-)?\w+@\w+.\w+$/;
+var email = ["mymail@mail.ru", "my.mail@mail.ru", "my-mail@mail.ru"];
+var textRegExp = /^.*$/;
+var text = "Произвольный текст";
