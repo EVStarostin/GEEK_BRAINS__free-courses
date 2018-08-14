3c98ffb2e8b71f5197e67afb529b958b9ca58505
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-14 13:46:27 +0300

    -

--- js2/lesson 3/home work/index.js
+++ js2/lesson 3/home work/index.js
@@ -1,11 +1,15 @@
-var quotesRegExp = /(\W')|('\W)/;
-var qoutesString = "Большой 'текст', где используются 'кавычки', но в слове aren’t 'кавычка' не 'кавычка', а 'апостроф'.";
+var quotesRegExp = /'(.+?)'(?=\W)/g;
+var quotesBtn = document.querySelector('#qoutesBtn').onclick = function() {
+    var qoutesStr = document.querySelector('#qoutesStr').value;
+    qoutesStr = qoutesStr.replace(quotesRegExp, '"$1"')
+    document.querySelector('#qoutesStr').value = qoutesStr;
+}
 
 var phoneRegExp = /^\+7\(\d{3}\)\d{3}-\d{4}$/;
-var phone = "+7(927)099-6701";
+var phoneStr = "+7(927)099-6701";
 
 var emailRegExp = /^\w+(\.|-)?\w+@\w+.\w+$/;
-var email = ["mymail@mail.ru", "my.mail@mail.ru", "my-mail@mail.ru"];
+var emailStr = ["mymail@mail.ru", "my.mail@mail.ru", "my-mail@mail.ru"];
 
 var textRegExp = /^.*$/;
-var text = "Произвольный текст";
+var textStr = "Произвольный текст";
