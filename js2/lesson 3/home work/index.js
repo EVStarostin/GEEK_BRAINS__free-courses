7bd62c7d4e284b66d99bcae76b9f629f64de4c76
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-14 17:28:20 +0300

    -

--- js2/lesson 3/home work/index.js
+++ js2/lesson 3/home work/index.js
@@ -22,26 +22,25 @@ function validateForm(e) {
   if (!PHONE_REG_EXP.test(phone.value)) validationErrors.push(phone);
   if (!EMAIL_REG_EXP.test(email.value)) validationErrors.push(email);
 
-  if (validationErrors.length > 0) e.preventDefault();
-  fields.forEach(element => {
-    if (validationErrors.indexOf(element) === -1) {
-      element.classList.remove('is-invalid');
-      element.classList.add('is-valid');
-      document.querySelector('#'+element.getAttribute('aria-describedby')).classList.remove('text-muted');
-      document.querySelector('#'+element.getAttribute('aria-describedby')).classList.remove('text-danger');
-      document.querySelector('#'+element.getAttribute('aria-describedby')).classList.add('text-success');
-    } else {
-      element.classList.remove('is-valid');
-      element.classList.add('is-invalid');
-      document.querySelector('#'+element.getAttribute('aria-describedby')).classList.remove('text-muted');
-      document.querySelector('#'+element.getAttribute('aria-describedby')).classList.remove('text-success');
-      document.querySelector('#'+element.getAttribute('aria-describedby')).classList.add('text-danger');
-    }
-  });
+  if (validationErrors.length > 0) {
+    e.preventDefault();
 
-  // validationErrors.forEach(element => {
-  //   element.classList.add('is-invalid');
-  //   document.querySelector('#'+element.getAttribute('aria-describedby')).classList.remove('text-muted');
-  //   document.querySelector('#'+element.getAttribute('aria-describedby')).classList.add('text-danger');
-  // });
+    fields.forEach(element => {
+      if (validationErrors.indexOf(element) === -1) {
+        element.classList.remove('is-invalid');
+        element.classList.add('is-valid');
+        document.querySelector('#'+element.getAttribute('aria-describedby')).classList.remove('text-muted');
+        document.querySelector('#'+element.getAttribute('aria-describedby')).classList.remove('text-danger');
+        document.querySelector('#'+element.getAttribute('aria-describedby')).classList.add('text-success');
+      } else {
+        element.classList.remove('is-valid');
+        element.classList.add('is-invalid');
+        document.querySelector('#'+element.getAttribute('aria-describedby')).classList.remove('text-muted');
+        document.querySelector('#'+element.getAttribute('aria-describedby')).classList.remove('text-success');
+        document.querySelector('#'+element.getAttribute('aria-describedby')).classList.add('text-danger');
+      }
+    });
+  } else {
+    alert("Форма отправлена!")
+  }
 }
