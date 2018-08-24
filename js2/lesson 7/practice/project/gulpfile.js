ca0c3b5d1554336f7674c2df67243109b8342360
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-24 17:51:22 +0300

    +

--- js2/lesson 7/practice/project/gulpfile.js
+++ js2/lesson 7/practice/project/gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
-// подключаем gulp-sass
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss')
@@ -27,4 +29,16 @@ gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
 });
+
+gulp.task('build', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Минифицируем только CSS файлы
+    .pipe(gulpIf('*.css', minifyCSS()))
+    // Uglifies only if it's a Javascript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
+  
+  
   
