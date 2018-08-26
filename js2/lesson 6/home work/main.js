1b4a5668541b5a2e474a0669849cd4f5590a3060
Evgenii Starostin <e.starostin89@gmail.com>
2018-08-26 20:58:04 +0300

    +

--- js2/lesson 6/home work/main.js
+++ js2/lesson 6/home work/main.js
@@ -6,13 +6,43 @@ $( function() {
         firstDay:1,
         dateFormat:"dd.mm.yy"
     });
+
+    $('.cart .title').on('click', () => {
+        $('.cart').toggleClass('show-items');
+    })
+
+    $('.product').draggable({
+        revert: true, 
+        revertDuration: 1,
+        stack: '.cart',
+        scroll: false,
+        start: function() {
+            $(this).css("transform", "scale(.5)")
+        },
+        stop: function() {
+            $(this).css("transform", "")
+        }
+    });
+    
+    $('.cart').droppable({
+        accept: ".product",
+        over: function(){
+            $(this).css("background-color","#d7fa99");
+        },
+        out: function(){
+            $(this).css("background-color","");
+        },
+        drop: function( event, ui ){
+            // alert(`Вы перетащили элемент ${ui.draggable} в область ${$(this)}.`);
+            $(this).css("background-color","");
+            const product = new Product($(ui.draggable).find('.name').text(), parseFloat($(ui.draggable).find('.price').text()));
+            cart.addProduct(product.getProductData());
+        }
+    });
 } );
 
 function applyjQueryUIEffect(field) {
     $(field).effect( "bounce", "slow" );
     $("#dialog").dialog( {modal:true, draggable:false, resizable: false} );
 }
-
-$('.cart .title').on('click', () => {
-    $('.cart').toggleClass('show-items');
-})
+    
