1f1adad9fbb5a1ead546814d2fdb30ffa77d29d0
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-28 17:16:11 +0300

    +

---
+++ js2/lesson 7/home work/src/js/main.js
@@ -0,0 +1,57 @@
+import $ from 'jquery';
+import 'jquery-ui-bundle';
+import 'popper.js'
+import 'bootstrap'
+
+import './validate';
+import './autocomplete'
+import {Product, cart} from './cart'
+
+$( function() {
+    $("#datepicker").datepicker({
+        monthNames:["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь"
+        ,"Ноябрь","Декабрь"],
+        dayNamesMin:["Вс","Пн","Вт","Ср","Чт","Пт","Сб"],
+        firstDay:1,
+        dateFormat:"dd.mm.yy"
+    });
+
+    $('.cart .title').on('click', () => {
+        $('.cart').toggleClass('show-items');
+    })
+
+    $('.product').draggable({
+        revert: true, 
+        revertDuration: 1,
+        stack: '.cart',
+        scroll: false,
+        start: function() {
+            $(this).css("transform", "scale(.5)")
+        },
+        stop: function() {
+            $(this).css("transform", "")
+        }
+    });
+    
+    $('.cart').droppable({
+        accept: ".product",
+        over: function(){
+            $(this).css("background-color","#d7fa99");
+        },
+        out: function(){
+            $(this).css("background-color","");
+        },
+        drop: function( event, ui ){
+            // alert(`Вы перетащили элемент ${ui.draggable} в область ${$(this)}.`);
+            $(this).css("background-color","");
+            const product = new Product($(ui.draggable).find('.name').text(), parseFloat($(ui.draggable).find('.price').text()));
+            cart.addProduct(product.getProductData());
+        }
+    });
+} );
+
+export function applyjQueryUIEffect(field) {
+    $(field).effect( "bounce", "slow" );
+    $("#dialog").dialog( {modal:true, draggable:false, resizable: false} );
+}
+    
---
+++ js2/lesson 7/home work/webpack.config.js
@@ -0,0 +1,49 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  optimization: {
+    minimizer: [
+        new UglifyJsPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: true // set to true if you want JS source maps
+        }),
+        new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackPlugin({  // Also generate a test.html
+        filename: 'index.html',
+        template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+                name: 'img/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
