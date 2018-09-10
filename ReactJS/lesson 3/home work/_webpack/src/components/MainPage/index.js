b7c2a8dde4f85f1f7e1250956b74c09d4fc9547a
Starostin Evgenii <estarostin@bellintegrator.com>
2018-09-10 17:52:03 +0300

    +

--- ReactJS/lesson 3/home work/_webpack/src/components/MainPage/index.js
+++ ReactJS/lesson 3/home work/_webpack/src/components/MainPage/index.js
@@ -8,8 +8,18 @@ class MainPage extends Component {
 
   render() {
     const { articles, focused } = this.props;
-    const filteredArticles = (focused === 0) ? articles :
-      articles.filter(item => item.categoryId === focused);
+    let filteredArticles = [];
+    if (focused === 0) {
+      filteredArticles = articles.sort((a, b) => {
+        const dateParts1 = a.date.split('.').map(item => +item);
+        const dateParts2 = b.date.split('.').map(item => +item);
+        const date1 = new Date(dateParts1[2], dateParts1[1]-1, dateParts1[0]);
+        const date2 = new Date(dateParts2[2], dateParts2[1]-1, dateParts2[0]);
+        return date2 - date1;
+      })
+    } else {
+      filteredArticles = articles.filter(item => item.categoryId === focused);
+    }
 
     return (
       <main className="main-page">
