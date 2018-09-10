2df0f2efa72660722513daa7626b73a4dcb3bf7f
Starostin Evgenii <estarostin@bellintegrator.com>
2018-09-10 16:38:45 +0300

    +

--- ReactJS/lesson 3/home work/_webpack/src/components/Layout/index.js
+++ ReactJS/lesson 3/home work/_webpack/src/components/Layout/index.js
@@ -6,17 +6,33 @@ import Footer from '../Footer';
 import Modal from '../Modal';
 import './style.css';
 
+import categories from '../../../mock/categories';
+import articles from '../../../mock/articles';
+
 class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {focused: 0};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(index, e){
+    e.preventDefault();
+    this.setState({focused: index});
+  }
+
   render() {
+    const { focused } = this.state;
+    const menuItems = [{id: 0, name: 'Главная'}].concat(categories);
     return (
       <div className="Layout">
         <div className="wrapper">
           <Header />
-          <Menu />
-          <MainPage />
+          <Menu items={menuItems} handleClick={this.handleClick} focused={focused} />
+          <MainPage articles={articles} focused={focused} />
         </div>
         <Footer />
-        <Modal />        
+        <Modal />
       </div>
     );
   }
--- ReactJS/lesson 3/home work/_webpack/src/components/Menu/index.js
+++ ReactJS/lesson 3/home work/_webpack/src/components/Menu/index.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { handleClick, items, focused } = this.props;
     return (
       <nav className="nav">
         <div className="container">
           <ul className="nav-menu">
-            <li className="nav-menu__item focused">
-              <a href="#">Главная</a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="#">Важное</a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="#">Слово Москве</a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="#">ЖЖизнь</a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="#">СССР</a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="#">Общество</a>
-            </li>
-            <li className="nav-menu__item">
-              <a href="#">Рубль</a>
-            </li>
+            {
+              items.map((item, index) => {
+                let style = 'nav-menu__item';
+                if (focused === index) {
+                  style = 'nav-menu__item focused';
+                }
+                return (
+                  <li className={style} key={index}>
+                    <a href="#" onClick={handleClick.bind(null, item.id)}>
+                      {item.name}
+                    </a>
+                  </li>
+                )
+              })
+            }
           </ul>
         </div>
       </nav>
