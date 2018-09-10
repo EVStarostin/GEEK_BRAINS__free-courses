f91e4c60513bea1a4b9712b136f467deef41a851
Starostin Evgenii <estarostin@bellintegrator.com>
2018-09-10 13:33:23 +0300

    +

--- ReactJS/lesson 3/home work/_webpack/src/components/Footer/index.js
+++ ReactJS/lesson 3/home work/_webpack/src/components/Footer/index.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <footer class="footer">
-        <div class="container">
-          <ul class="footer-menu">
-            <li class="footer-menu__item"><a href="#">О проекте</a></li>
-            <li class="footer-menu__item"><a href="#">Пользовательское соглашение</a></li>
-            <li class="footer-menu__item"><a href="#">Помощь</a></li>
+      <footer className="footer">
+        <div className="container">
+          <ul className="footer-menu">
+            <li className="footer-menu__item"><a href="#">О проекте</a></li>
+            <li className="footer-menu__item"><a href="#">Пользовательское соглашение</a></li>
+            <li className="footer-menu__item"><a href="#">Помощь</a></li>
           </ul>
         </div>
       </footer>
--- ReactJS/lesson 3/home work/_webpack/src/components/Modal/index.js
+++ ReactJS/lesson 3/home work/_webpack/src/components/Modal/index.js
@@ -3,30 +3,30 @@ import React, { Component } from 'react';
 class Modal extends Component {
   render() {
     return (
-      <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="loginModalLabel">Войти</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <div className="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="loginModalLabel">Войти</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
               <form>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text" id="login-input"><i class="fas fa-user"></i></span>
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text" id="login-input"><i className="fas fa-user"></i></span>
                   </div>
-                  <input type="text" class="form-control" placeholder="Имя пользователя" aria-label="Username" aria-describedby="login-input"/>
+                  <input type="text" className="form-control" placeholder="Имя пользователя" aria-label="Username" aria-describedby="login-input"/>
                 </div>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text" id="password-input"><i class="fas fa-unlock-alt"></i></span>
+                <div className="input-group mb-3">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text" id="password-input"><i className="fas fa-unlock-alt"></i></span>
                   </div>
-                  <input type="text" class="form-control" placeholder="Пароль " aria-label="Password" aria-describedby="password-input"/>
+                  <input type="text" className="form-control" placeholder="Пароль " aria-label="Password" aria-describedby="password-input"/>
                 </div>
-                <button type="button" class="btn btn-primary float-right">Войти</button>
+                <button type="button" className="btn btn-primary float-right">Войти</button>
               </form>
             </div>
           </div>
